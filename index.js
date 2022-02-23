e60328471100799cbdc69753165a232581c780c2
Chester Heng <chester.heng@gmail.com>
2022-02-24 04:02:53 +0800

    parse payloads

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 const server = http.createServer(function (req, res) {
   // parsedUrl = http://localhost:3000/myapp/?search=test
@@ -27,7 +28,16 @@ const server = http.createServer(function (req, res) {
   // }
   const headers = req.headers;
 
-  res.end("Hello World\n");
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", function (data) {
+    buffer += decoder.write(data);
+  });
+  req.on("end", function () {
+    buffer += decoder.end();
+    res.end("Hello World\n");
+    console.log(buffer);
+  });
 });
 
 server.listen(3000, function () {
